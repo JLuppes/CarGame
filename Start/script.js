ac46e0bf5420dddaf2238a40cafa64c8605d62ef
Joshua Luppes <lupjos@dunwoody.edu>
2026-06-10 14:54:12 -0500

    Refactor app and instruction steps

---
+++ Start/script.js
@@ -0,0 +1,32 @@
+//////// STEP 4 (start)
+
+
+//////// STEP 4 (end)
+
+
+
+//////// STEP 5 (start)
+
+
+//////// STEP 5 (end)
+
+
+
+//////// STEP 6 (start)
+
+
+//////// STEP 6 (end)
+
+
+
+//////// STEP 7 (start)
+
+
+//////// STEP 7 (end)
+
+
+
+//////// STEP 8 (start)
+
+
+//////// STEP 8 (end)
